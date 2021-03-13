eba4b235a38c3505e498f6c2b25a5948300f12bb
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-03-13 15:14:50 +0100

    Creada libreria con los atributos constantes de nuestro juego

---
+++ src/constants/Constants.java
@@ -0,0 +1,20 @@
+package constants;
+
+public class Constants {
+
+    // Dimensiones de la ventana
+
+    public static final int WIDTH = 1000;                   //Alto de la ventana
+    public static final int HEIGHT = 600;                   //Ancho de la ventana
+
+    // Atributos del jugador
+
+    public static final int FIRERATE = 300;                 //Velocidad de disparo del jugador
+    public static final double DELTAANGLE = 0.1;            //Angulo de rotacion del jugador
+    public static final double ACC = 0.2;                   //Constante de aceleracion del jugador
+    public static final double PLAYER_MAX_VEL = 7.0;        //Velocidad maxima del jugador
+
+    // Atributos de los laseres
+
+    public static final double LASER_VEL = 15.0;            //Velocidad del laser
+}
